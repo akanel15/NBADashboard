6f1e1138b4a75883d247260a4c21838844d1faba
Luke Bonso <lbon0008@student.monash.edu>
2021-09-05 21:35:37 +1000

    Frontend-backend and backend to frontend coms

--- data_blueprint.py
+++ data_blueprint.py
@@ -3,10 +3,11 @@ from flask import Blueprint, Flask, render_template, request, jsonify, make_resp
 data = Blueprint('data', __name__, template_folder='templates')
 
 
-@data.route("/getdata")
+@data.route("/getdata", methods=["POST", "GET"])
 def test():
     #put the data into the array which you want to render on the page
-    array = getdata(Name, Team)
+    array = request.get_json()
+    print(array)
 
     res = make_response(jsonify(array), 200)
 
