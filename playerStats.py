1d462f85e13768fe5f10ac73d72ce29eda51fb5d
Pan <zpan0008@student.monash.edu>
2021-09-05 17:47:09 +1000

    python code to display a specific player's statistics, and shot area information in that team.

---
+++ playerStats.py
@@ -0,0 +1,27 @@
+from nba_api.stats.endpoints import playercareerstats, teamvsplayer, teamplayerdashboard
+from nba_api.stats.static import players
+from nba_api.stats.static import teams
+# get_teams returns a list of 30 dictionaries, each an NBA team.
+nba_teams = teams.get_teams()
+# get_players returns a list of dictionaries, each representing a player.
+nba_players = players.get_players()
+teamDash = teamplayerdashboard.TeamPlayerDashboard('1610612747').get_data_frames()[1]
+print(teamDash)
+
+print("Enter player's name to check his career stats!")
+playerToCheck = str(input())
+print("Enter player's current team!")
+currentTeam = str(input())
+
+current_team = [team for team in nba_teams
+                if team['full_name'] == currentTeam][0]
+
+selected_player = [player for player in nba_players
+                   if player['full_name'] == playerToCheck][0]
+
+career = playercareerstats.PlayerCareerStats(player_id=str(selected_player.get('id')))
+res = career.get_data_frames()[0]
+print(res)
+
+shotArea = teamvsplayer.TeamVsPlayer(team_id=str(current_team.get('id')), vs_player_id=str(selected_player.get('id')))
+print(shotArea.get_data_frames()[6])
