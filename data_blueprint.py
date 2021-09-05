bb9f4fe83eccbd297933baf8b8f92c6c1a49d0e3
Luke Bonso <lbon0008@student.monash.edu>
2021-09-05 16:24:44 +1000

    data response implemeneted

--- data_blueprint.py
+++ data_blueprint.py
@@ -3,6 +3,12 @@ from flask import Blueprint, Flask, render_template, request, jsonify, make_resp
 data = Blueprint('data', __name__, template_folder='templates')
 
 
-@data.route("/meme")
+@data.route("/getdata")
 def test():
-    print("h")
+    #put the data into the array which you want to render on the page
+    
+    array = []
+
+    res = make_response(jsonify(array), 200)
+
+    return res
