2688d2f8f24c64e67acb46f73f23a894ec0060e8
Zhongxun Pan(rym) <zpan0008@student.monash.edu>
2021-09-07 00:11:59 +1000

    Replace playerStats.py

--- playerStats.py
+++ playerStats.py
@@ -1,27 +1,27 @@
 from nba_api.stats.endpoints import playercareerstats, teamvsplayer, teamplayerdashboard
 from nba_api.stats.static import players
 from nba_api.stats.static import teams
+
 # get_teams returns a list of 30 dictionaries, each an NBA team.
 nba_teams = teams.get_teams()
 # get_players returns a list of dictionaries, each representing a player.
 nba_players = players.get_players()
-teamDash = teamplayerdashboard.TeamPlayerDashboard('1610612747').get_data_frames()[1]
-print(teamDash["PLAYER_ID"][10])
 
-print("Enter player's name to check his career stats!")
-playerToCheck = str(input())
-print("Enter player's current team!")
-currentTeam = str(input())
+# print("Enter your team name in full!")
+current_team = 'Los Angeles Lakers'  # str(input())
+inputTeam = [team for team in nba_teams
+             if team['full_name'] == current_team][0]
+teamDash = teamplayerdashboard.TeamPlayerDashboard(team_id=inputTeam.get('id')).get_data_frames()[1]
 
-current_team = [team for team in nba_teams
-                if team['full_name'] == currentTeam][0]
+playerId_List = []
+for player_id in teamDash['PLAYER_ID']:
+    playerId_List.append(player_id)
 
-selected_player = [player for player in nba_players
-                   if player['full_name'] == playerToCheck][0]
+player_ActiveYears = []
+player_Points = []
 
-career = playercareerstats.PlayerCareerStats(player_id=str(selected_player.get('id')))
-res = career.get_data_frames()[0]
-print(res)
+# for player_id in playerId_List:
+    # careerInfo = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()[0]
 
-shotArea = teamvsplayer.TeamVsPlayer(team_id=str(current_team.get('id')), vs_player_id=str(selected_player.get('id')))
-print(shotArea.get_data_frames()[6])
+# shotArea = teamvsplayer.TeamVsPlayer(team_id=str(current_team.get('id')), vs_player_id=str(selected_player.get('id')))
+# print(shotArea.get_data_frames()[6])
