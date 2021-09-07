a5c97547799a7c5a3add28721acfc6c61713a784
Jeremy Ong <jpongfam@gmail.com>
2021-09-07 17:38:35 +1000

    added sticky footer to bottom of page added css page

--- static/base.js
+++ static/base.js
@@ -20,14 +20,42 @@ function initialiseTheme() {
     theme = localStorage.getItem("theme")
     link = ""
 
-    if (theme == "light") {
+    if (theme == "dark") {
+        link = darkTheme
+    } else {
         link = lightTheme
     }
-    else if (theme == "dark") {
-        link = darkTheme
-    }
+
 
     document.getElementById("theme").href = link
 }
 
 initialiseTheme()
+
+
+// Event listener for mobile navbar so that it is toggable
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
