f545765ad108e4e688ecae4905b0cbaa1ada37de
Luke Bonso <lbon0008@student.monash.edu>
2021-09-08 09:04:41 +1000

    done

--- playerStats.py
+++ playerStats.py
@@ -7,11 +7,13 @@ nba_teams = teams.get_teams()
 # get_players returns a list of dictionaries, each representing a player.
 nba_players = players.get_players()
 
+
 # print("Enter your team name in full!")
 current_team = 'Los Angeles Lakers'  # str(input())
 inputTeam = [team for team in nba_teams
              if team['full_name'] == current_team][0]
 teamDash = teamplayerdashboard.TeamPlayerDashboard(team_id=inputTeam.get('id')).get_data_frames()[1]
+
 
 playerId_List = []
 for player_id in teamDash['PLAYER_ID']:
@@ -27,7 +29,7 @@ def player_info(pid):
         player_ActiveYears.append(year)
     for pts in career.get_data_frames()[0]['PTS']:
         player_Points.append(pts)
-    return player_ActiveYears, player_Points
+    return [player_ActiveYears, player_Points]
 
 
 # LeBron James information.
