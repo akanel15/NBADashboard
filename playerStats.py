b552540c850308a3ea398a8a7ebff9b0957a09f0
Pan <zpan0008@student.monash.edu>
2021-09-09 10:14:11 +1000

    neater code, new method called player in the team to return all the players in the team.

--- playerStats.py
+++ playerStats.py
@@ -2,34 +2,31 @@ from nba_api.stats.endpoints import playercareerstats, teamplayerdashboard
 from nba_api.stats.static import players
 from nba_api.stats.static import teams
 
-# get_teams returns a list of 30 dictionaries, each an NBA team.
-nba_teams = teams.get_teams()
-# get_players returns a list of dictionaries, each representing a player.
+
+def players_in_the_team(teamName):
+    player_name = []
+    nba_teams = teams.get_teams()
+
+    selected_team = [team for team in nba_teams
+                     if team['full_name'] == teamName][0]
+
+    info = teamplayerdashboard.TeamPlayerDashboard(team_id=selected_team.get('id'))
+
+    for player in info.get_data_frames()[1]['PLAYER_NAME']:
+        player_name.append(player)
+
+    return player_name
 
 
-# print("Enter your team name in full!")
-current_team = 'Los Angeles Lakers'  # str(input())
-inputTeam = [team for team in nba_teams
-             if team['full_name'] == current_team][0]
-teamDash = teamplayerdashboard.TeamPlayerDashboard(team_id=inputTeam.get('id')).get_data_frames()[1]
-
-playerId_List = []
-for player_id in teamDash['PLAYER_ID']:
-    playerId_List.append(player_id)
-
-playerToCheck = "Stephen Curry"
-
-
-def get_ID(playerToCheck):
+def get_ID(playerName):
     nba_players = players.get_players()
 
     selected_player = [player for player in nba_players
-                       if player['full_name'] == playerToCheck][0]
+                       if player['full_name'] == playerName][0]
 
     career = playercareerstats.PlayerCareerStats(player_id=str(selected_player.get('id')))
-    res = career.get_data_frames()[0]
 
-    return res.PLAYER_ID[0]
+    return career.get_data_frames()[0].PLAYER_ID[0]
 
 
 def player_info(player):
@@ -40,8 +37,11 @@ def player_info(player):
     player_Assists = []
     player_Steals = []
     player_Blocks = []
+
     pid = get_ID(player)
+
     career = playercareerstats.PlayerCareerStats(player_id=pid)
+
     for year in career.get_data_frames()[0]['SEASON_ID']:
         player_ActiveYears.append(year)
     for pts in career.get_data_frames()[0]['PTS']:
@@ -57,4 +57,5 @@ def player_info(player):
     for blk in career.get_data_frames()[0]['BLK']:
         player_Blocks.append(blk)
 
-    return [player_ActiveYears, player_Points, player_Gameplayed, player_Rebounds, player_Assists, player_Steals, player_Blocks]
+    return [player_ActiveYears, player_Points, player_Gameplayed, player_Rebounds, player_Assists, player_Steals,
+            player_Blocks]
