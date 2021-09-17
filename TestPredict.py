8615d5186d5219136c436e5e56b02eb789a9ec0a
zpan0008 <zpan0008@student.monash.edu>
2021-09-17 23:40:52 +1000

    setting up the CI/CD pipeline plus test file.

---
+++ TestPredict.py
@@ -0,0 +1,9 @@
+from predict import predict
+import unittest
+
+class TestPredict(unittest.TestCase):
+    def test_predict(self):
+        pass
+
+    if __name__ == "__main__":
+        unittest.main(verbosity=2)
