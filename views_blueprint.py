9e35b25480e64452710196a2c6e3b2feb2e6c30c
Jeremy Ong <jong0053@student.monash.edu>
2021-09-18 16:35:27 +1000

    working on team page

--- views_blueprint.py
+++ views_blueprint.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, Flask, render_template, request, jsonify, make_response
+from flask import *
+from nba_api.stats.static import teams
+from teamStats import get_team_info_using_teamid
+
 
 views = Blueprint('views', __name__, template_folder='templates')
 
@@ -12,6 +15,9 @@ def home():
 def player():
     return render_template('player.html')
 
-@views.route("/team")
-def team():
-    return render_template('team.html')
+
+@views.route("/team/<teamID>")
+def team(teamID):
+    teamData = get_team_info_using_teamid(teamID)
+    teamInfo = teams.find_team_name_by_id(teamID)
+    return render_template('team.html', teamInfo=teamInfo, teamData=teamData)
