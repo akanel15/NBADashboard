dd5ab8783637de54a685f4e9f53bed7efcc94e4a
Jeremy Ong <jong0053@student.monash.edu>
2021-09-18 17:58:48 +1000

    pass team data from backend into team.js

---
+++ static/team.js
@@ -0,0 +1,11 @@
+
+// Call all functions from here
+function teamPage(teamData) {
+    example(teamData)
+}
+
+// This is an example on how to use team data
+// call this function from the teamPage function
+function example(data) {
+    console.log(data)
+}
