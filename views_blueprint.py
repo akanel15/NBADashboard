9136e0879eb75b102acbc6b60a024e3b2c045664
Jeremy Ong <jpongfam@gmail.com>
2021-09-19 23:52:46 +1000

    changed url for team page to team full name

--- views_blueprint.py
+++ views_blueprint.py
@@ -1,6 +1,6 @@
 from flask import *
 from nba_api.stats.static import teams
-from teamStats import get_team_info_using_teamid
+from teamStats import get_team_info, get_team_info_using_teamid
 
 
 views = Blueprint('views', __name__, template_folder='templates')
@@ -16,8 +16,9 @@ def player():
     return render_template('player.html')
 
 
-@views.route("/team/<teamID>")
-def team(teamID):
-    teamData = get_team_info_using_teamid(teamID)
-    teamInfo = teams.find_team_name_by_id(teamID)
+@views.route("/team/<teamRequest>")
+def team(teamRequest):
+    teamName = teamRequest.replace("%20", " ")
+    teamData = get_team_info(teamName)
+    teamInfo = teams.find_teams_by_full_name(teamName)[0]
     return render_template('team.html', teamInfo=teamInfo, teamData=teamData)
