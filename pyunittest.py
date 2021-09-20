6bd9b10560da056dccc7ce18018ba4e28851f463
zpan0008 <zpan0008@student.monash.edu>
2021-09-21 04:30:40 +1000

    100% cover for predict.py and rating_calc.py.

--- pyunittest.py
+++ pyunittest.py
@@ -1,7 +1,7 @@
 import unittest
 
 from predict import player_predictor
-
+from playerStats import defensive_rating_calc, offensive_rating_calc
 
 class TestPredict(unittest.TestCase):
     def test_predict_lebron_james_pts(self):
@@ -28,6 +28,24 @@ class TestPredict(unittest.TestCase):
              7.2894736842105265, 7.468354430379747, 7.935483870967742, 8.026315789473685, 6.922077922077922, 
              6.028985507246377, 7.434210526315789, 8.635135135135135, 8.646341463414634, 8.454545454545455, 
              7.835820895522388, 7.688888888888889, 7.441, 7.171], 0.3), [6.817, 6.515])
+
+    def test_defensive_rating_calc_stl(self):
+        self.assertEqual(defensive_rating_calc(1.4, 1.2), 98)
+        self.assertEqual(defensive_rating_calc(1.2, 1.2), 96)
+
+    def test_defensive_rating_calc_blk(self):
+        self.assertEqual(defensive_rating_calc(0.8, 1.4), 90)
+        self.assertEqual(defensive_rating_calc(0.8, 1.2), 88)
+
+    def test_offensive_rating_calc(self):
+        self.assertEqual(offensive_rating_calc(29, 5, 5, 0.4), 92)
+        self.assertEqual(offensive_rating_calc(27, 5, 5, 0.4), 91)
+        self.assertEqual(offensive_rating_calc(29, 9, 5, 0.4), 95)
+        self.assertEqual(offensive_rating_calc(29, 7, 5, 0.4), 94)
+        self.assertEqual(offensive_rating_calc(29, 5, 12, 0.4), 96)
+        self.assertEqual(offensive_rating_calc(29, 5, 10, 0.4), 96)
+        self.assertEqual(offensive_rating_calc(29, 5, 11, 0.6), 97)
+        self.assertEqual(offensive_rating_calc(29, 5, 11, 0.4), 96)
 
 if __name__ == "__main__":
     unittest.main()
