743e12c4a6d00196aecfe04aeacffbdec84a8cf6
Luke Bonso <lbon0008@student.monash.edu>
2021-10-03 21:14:31 +1100

    Trying autoload

--- static/team.js
+++ static/team.js
@@ -1,7 +1,10 @@
 
+
 // Call all functions from here
-function teamPage(teamData) {
-    // example(teamData)
+function teamPage(teamData, name) {
+    window.localStorage.setItem("current_team", JSON.stringify([name]))
+    window.localStorage.setItem("current_page", JSON.stringify({team: true, player: false}))
+
 }
 
 // // This is an example on how to use team data
