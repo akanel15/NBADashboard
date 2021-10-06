eebcfafe3790b7c9c42cf1d54c69a8e16f243485
zpan0008 <zpan0008@student.monash.edu>
2021-10-07 09:27:57 +1100

    restoring get_team_info_using_teamid

--- teamStats.py
+++ teamStats.py
@@ -149,3 +149,51 @@ def get_team_info(teamName):
            [player_name], \
            [last_5_games, w, l, w_pct, fga, fgm, fg_pct, pts, ast, reb, stl, blk, tov, plus_minus], \
            [shot_area, fga_area, fgm_area, fg_pct_area]
+
+
+def get_team_info_using_teamid(teamID):
+    team_stats = teamyearbyyearstats.TeamYearByYearStats(teamID).get_data_frames()[0]
+
+    season_played = []
+    for season in team_stats['YEAR']:
+        season_played.append(season)
+
+    game_played = []
+    for gp in team_stats['GP']:
+        game_played.append(gp)
+
+    wins = []
+    for win in team_stats['WINS']:
+        wins.append(win)
+
+    losses = []
+    for loss in team_stats['LOSSES']:
+        losses.append(loss)
+
+    win_percentage = []
+    for win_pct in team_stats['WIN_PCT']:
+        win_percentage.append(win_pct)
+
+    conf_rank = []
+    for _ in team_stats['CONF_RANK']:
+        conf_rank.append(_)
+
+    po_wins = []
+    for powins in team_stats['PO_WINS']:
+        po_wins.append(powins)
+
+    po_losses = []
+    for polosses in team_stats['PO_LOSSES']:
+        po_losses.append(polosses)
+
+    # N/A - not in final, FINALS APPEARANCE - lost in final, LEAGUE CHAMPION - won in final.
+    final_appearance = []
+    for _ in team_stats['NBA_FINALS_APPEARANCE']:
+        final_appearance.append(_)
+
+    po_gp = [[] for _ in po_wins]
+    for i in range(len(po_gp)):
+        po_gp[i] = po_wins[i] + po_losses[i]
+
+    return [season_played, game_played, wins, losses, win_percentage, conf_rank, po_gp, po_wins, po_losses,
+            final_appearance]
