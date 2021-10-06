a0f230acbfc71e3bb8d957d8065603bdaa7bd6c0
zpan0008 <zpan0008@student.monash.edu>
2021-10-07 09:00:04 +1100

    team info, getting all players' names and team last 5 games stats.

--- teamStats.py
+++ teamStats.py
@@ -1,4 +1,4 @@
-from nba_api.stats.endpoints import teamyearbyyearstats
+from nba_api.stats.endpoints import teamyearbyyearstats, teamplayerdashboard, teamdashboardbylastngames
 from nba_api.stats.static import teams
 
 
@@ -6,8 +6,7 @@ def get_team_id(teamName):
     nba_teams = teams.get_teams()
 
     selected_team = [team for team in nba_teams
-                    if team['full_name'] == teamName][0]
-
+                     if team['full_name'] == teamName][0]
 
     teamInfo = teamyearbyyearstats.TeamYearByYearStats(team_id=str(selected_team.get('id')))
 
@@ -15,7 +14,6 @@ def get_team_id(teamName):
 
 
 def get_team_info(teamName):
-
     team_id = get_team_id(teamName)
 
     team_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=team_id).get_data_frames()[0]
@@ -61,12 +59,41 @@ def get_team_info(teamName):
     for i in range(len(po_gp)):
         po_gp[i] = po_wins[i] + po_losses[i]
 
+    latest_season = season_played[-1]
+    team_dashboard = teamplayerdashboard.TeamPlayerDashboard(team_id=team_id, season=latest_season).get_data_frames()[1]
+
+    player_name = []
+    for player in team_dashboard['PLAYER_NAME']:
+        player_name.append(player)
+
+    team_lastgame = teamdashboardbylastngames.TeamDashboardByLastNGames(
+        team_id=team_id, season=latest_season).get_data_frames()[1]
+
+    last_5_games = team_lastgame['GROUP_VALUES']
+    w = team_lastgame['W']
+    l = team_lastgame['L']
+    w_pct = team_lastgame['W_PCT']
+    # field goal attempt
+    fga = team_lastgame['FGA']
+    # field goal made
+    fgm = team_lastgame['FGM']
+    fg_pct = team_lastgame['FG_PCT']
+    reb = team_lastgame['REB']
+    ast = team_lastgame['AST']
+    # turnovers
+    tov = team_lastgame['TOV']
+    stl = team_lastgame['STL']
+    blk = team_lastgame['BLK']
+    pts = team_lastgame['PTS']
+    # team efficiency
+    plus_minus = team_lastgame['PLUS_MINUS']
     return [season_played, game_played, wins, losses, win_percentage, conf_rank, po_gp, po_wins, po_losses,
-            final_appearance]
+            final_appearance], \
+           [player_name], \
+           [last_5_games, w, l, w_pct, fga, fgm, fg_pct, pts, ast, reb, stl, blk, tov, plus_minus]
 
 
 def get_team_info_using_teamid(teamID):
-
     team_stats = teamyearbyyearstats.TeamYearByYearStats(teamID).get_data_frames()[0]
 
     season_played = []
